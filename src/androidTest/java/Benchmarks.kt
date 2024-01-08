eb6f64f11767ace392a1f0b499d88d4b11b7100d
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2024-01-08 15:26:18 +0100

    Add benchmarks writing to a file channel

--- src/androidTest/java/Benchmarks.kt
+++ src/androidTest/java/Benchmarks.kt
@@ -7,15 +7,21 @@ package org.example
 import androidx.benchmark.junit4.BenchmarkRule
 import androidx.benchmark.junit4.measureRepeated
 import androidx.test.runner.AndroidJUnit4
+import kotlinx.io.Buffer
 import kotlinx.io.buffered
 import kotlinx.io.discardingSink
 import kotlinx.io.files.Path
 import kotlinx.io.files.SystemFileSystem
+import kotlinx.io.write
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.kotlinx.io.encodeToBufferedSink
+import org.junit.After
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.nio.channels.FileChannel
+import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
 
 @RunWith(AndroidJUnit4::class)
 open class Benchmarks {
@@ -30,7 +36,13 @@ open class Benchmarks {
         Benchmarks::class.java.getResource("/citm_catalog.json").readBytes().decodeToString())
     private val sink = discardingSink().buffered()
     private val fileSink = SystemFileSystem.sink(Path("/dev/null")).buffered()
+    private val fileChannelSink = FileChannel.open(Paths.get("/dev/null"), StandardOpenOption.WRITE)
 
+    @After
+    fun closeFiles() {
+        fileSink.close()
+        fileChannelSink.close()
+    }
 
     @Test
     fun testTwitterMacro() {
@@ -74,6 +86,33 @@ open class Benchmarks {
         benchmarkingRule.measureRepeated {
             Json.encodeToBufferedSink(CitmCatalog.serializer(), citm, fileSink)
             fileSink.flush()
+        }
+    }
+
+    @Test
+    fun testTwitterMacroFileChannel() {
+        benchmarkingRule.measureRepeated {
+            val buffer = Buffer()
+            Json.encodeToBufferedSink(MacroTwitterFeed.serializer(), twitterMacro, buffer)
+            fileChannelSink.write(buffer)
+        }
+    }
+
+    @Test
+    fun testTwitterFileChannel() {
+        benchmarkingRule.measureRepeated {
+            val buffer = Buffer()
+            Json.encodeToBufferedSink(Twitter.serializer(), twitter, buffer)
+            fileChannelSink.write(buffer)
+        }
+    }
+
+    @Test
+    fun testCitmFileChannel() {
+        benchmarkingRule.measureRepeated {
+            val buffer = Buffer()
+            Json.encodeToBufferedSink(CitmCatalog.serializer(), citm, buffer)
+            fileChannelSink.write(buffer)
         }
     }
 }
